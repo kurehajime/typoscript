f78ec01a441d7e01ee6108a83147dbaa0d50dff4
kurehajime <xiidec@gmail.com>
2021-12-12 12:13:57 +0000

    ghpages

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const stylesHandler = "style-loader";
 const config = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
   devServer: {
     open: true,
